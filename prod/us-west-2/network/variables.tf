83aeb99c1e6a9a57bba8a75608c1b9832ca50e41
Ian Groff <ian@iangroff.com>
2020-01-17 17:03:17 -0600

    use all the AZs

--- prod/us-west-2/network/variables.tf
+++ prod/us-west-2/network/variables.tf
@@ -14,17 +14,17 @@ locals  {
     two = {
       name: "primary-2"
       cidr: "192.168.13.0/24"
-      az: "${var.aws_region}a"
+      az: "${var.aws_region}b"
     }
     three = {
       name: "primary-3"
       cidr: "192.168.14.0/24"
-      az: "${var.aws_region}b"
+      az: "${var.aws_region}c"
     }
     four = {
       name: "primary-4"
       cidr: "192.168.15.0/24"
-      az: "${var.aws_region}b"
+      az: "${var.aws_region}d"
     }
   }
 }
