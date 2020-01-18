9ebac165695073130681c617966543a214453b0b
Ian Groff <IanAtIanGroffDotCom>
2020-01-17 21:05:01 -0600

    switching to config driven from hardcoded values

--- prod/us-east-2/network/main.tf
+++ prod/us-east-2/network/main.tf
@@ -3,51 +3,31 @@ terraform {
   backend "s3" {}
 }
 
-
 provider "aws" {
   region = var.aws_region
   profile = var.aws_profile
 }
 
+locals {
+  network_config = var.network_configs[var.aws_region]
+}
+
 resource "aws_vpc" "primary" {
-  cidr_block            = "192.168.12.0/22"
+  cidr_block            = local.network_config.vpc.cidr_block
   enable_dns_hostnames  = true
   tags = {
     Name = "primary"
   }
 }
 
-resource "aws_subnet" "primary-sn-1" {
-  vpc_id     = aws_vpc.primary.id
-  cidr_block = "192.168.12.0/24"
+resource "aws_subnet" "primary_subnets" {
+  for_each = local.network_config.subnets
+
+  vpc_id = aws_vpc.primary.id
+  cidr_block = each.value["cidr"]
+  availability_zone = each.value["az"]
 
   tags = {
-    Name = "primary-1"
-  }
-}
-
-resource "aws_subnet" "primary-sn-2" {
-  vpc_id     = aws_vpc.primary.id
-  cidr_block = "192.168.13.0/24"
-
-  tags = {
-    Name = "primary-2"
-  }
-}
-
-resource "aws_subnet" "primary-sn-3" {
-  vpc_id     = aws_vpc.primary.id
-  cidr_block = "192.168.14.0/24"
-
-  tags = {
-    Name = "primary-3"
-  }
-}
-resource "aws_subnet" "primary-sn-4" {
-  vpc_id     = aws_vpc.primary.id
-  cidr_block = "192.168.15.0/24"
-
-  tags = {
-    Name = "primary-4"
+    Name = "${each.key}"
   }
 }
--- prod/us-east-2/network/variables.tf
+++ prod/us-east-2/network/variables.tf
@@ -3,3 +3,5 @@ variable "aws_region" {}
 variable "aws_profile" {
     type = string
 }
+
+variable "network_configs" {}
